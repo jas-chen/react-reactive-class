d743861565af809f28ef7e20b735899378c0e020
Jas Chen <jas228@gmail.com>
2015-09-19 08:19:46 +0800

    remove decorator config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import createReactiveClass from './createReactiveClass';
 
-export function reactive() {
-  // config via decorator
-  if (typeof arguments[0] === 'string') {
-    return (target) => createReactiveClass(target, ...arguments);
-  }
-
-  return createReactiveClass(...arguments);
+export function reactive(reactClass) {
+  return createReactiveClass(reactClass);
 }
 
 export const dom = Object.keys(React.DOM).reduce((result, tag) => {

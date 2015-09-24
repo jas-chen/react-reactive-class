3814c500a1dc6009bfc63aa37b0e2b1265c17e19
Jas Chen <jas228@gmail.com>
2015-09-24 08:26:31 +0800

    v1.0.1
    Reactive component will now re-render only when state updates

--- src/createReactiveClass.js
+++ src/createReactiveClass.js
@@ -18,6 +18,10 @@ export default function createReactiveClass(tag) {
       this.subscribe(nextProps);
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+      return nextState !== this.state;
+    }
+
     componentWillUnmount() {
       this.unsubscribe();
     }
